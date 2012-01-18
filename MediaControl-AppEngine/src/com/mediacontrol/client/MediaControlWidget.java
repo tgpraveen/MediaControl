150865c8dfac2cf8e88515295260c8a2aebc6ea9
Praveen Thirukonda <tgpraveen89@gmail.com>
2012-01-18 12:49:24 -0500

    One more line commented out to implemnt previous commit's changes.

--- MediaControl-AppEngine/src/com/mediacontrol/client/MediaControlWidget.java
+++ MediaControl-AppEngine/src/com/mediacontrol/client/MediaControlWidget.java
@@ -17,7 +17,7 @@ package com.mediacontrol.client;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.InputElement;
-import com.google.gwt.dom.client.TextAreaElement;
+//import com.google.gwt.dom.client.TextAreaElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
